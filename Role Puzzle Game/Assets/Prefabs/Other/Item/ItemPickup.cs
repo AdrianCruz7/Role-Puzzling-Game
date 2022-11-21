f790192fb3dbb8e67c8cbf37eba3156bfccb4ec5
MatthewM5293 <mmunozramirez@student.neumont.edu>
2022-11-21 09:09:33 -0700

    Added Item Collision

--- Role Puzzle Game/Assets/Prefabs/Other/Item/ItemPickup.cs
+++ Role Puzzle Game/Assets/Prefabs/Other/Item/ItemPickup.cs
@@ -20,4 +20,11 @@ public class ItemPickup : MonoBehaviour
         }
         
     }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player")
+        {
+            Pickup();
+        }
+    }
 }
