b363dd6b9080eaa62c4c58804824406fc7b6ae7c
acruz1 <acruz@student.neumont.edu>
2022-12-03 23:27:25 -0700

    Added Audiomanager and music/sfx

--- Role Puzzle Game/Assets/Prefabs/Characters/Player/PlayerController.cs
+++ Role Puzzle Game/Assets/Prefabs/Characters/Player/PlayerController.cs
@@ -103,6 +103,7 @@ public class PlayerController : MonoBehaviour
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+        AudioManager.instance.WalkSFX();
     }
 
     //Inputsystem Function
